5340cab9be983dea4653aca5f8df515eae8de4ef
jengkolina@gmail.com <jengkolina@gmail.com>
2025-05-24 10:29:30 +0000

    1) moved staging tables to jaffle_stg folder models, and materialized them as views. 2) Changed config of materialized tables n views on dbt_project.yml.  3) Removed sample models

--- models/customer.sql
+++ models/customer.sql
@@ -1,11 +1,3 @@
-{{
-  config(
-    materialized='view'
-  )
-}}
-
-
-
 with customers as ( select * from {{ref('stg_customers')}} ),
 orders as ( select * from {{ref('stg_orders')}}),
 customer_orders as (
